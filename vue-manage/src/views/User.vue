8444230d8ee08eef3dd5b366143ef5175720af25
qqh6666666 <qian1234569@qq.com>
2023-03-11 16:46:12 +0800

    mock获取用户数据 2023/3/11

--- vue-manage/src/views/User.vue
+++ vue-manage/src/views/User.vue
@@ -1,11 +1,108 @@
 <template>
-  <h1>User</h1>
+  <div class="manage">
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="50%" :before-close="handleClose">
+      <!-- 用户表单信息 -->
+      <el-form ref="form" :rules="rules" :inline="true" :model="form" label-width="80px">
+        <el-form-item label="姓名" prop="name">
+          <el-input placeholder="请输入姓名" v-model="form.name"></el-input>
+        </el-form-item>
+
+        <el-form-item label="年龄" prop="age">
+          <el-input placeholder="请输入年龄" v-model="form.age"></el-input>
+        </el-form-item>
+
+        <el-form-item label="性别" prop="sex">
+          <el-select v-model="form.sex" placeholder="请选择性别">
+            <el-option label="男" value="1"></el-option>
+            <el-option label="女" value="0"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="出生日期" prop="birth">
+          <el-date-picker v-model="form.birth" type="date" placeholder="选择日期"> </el-date-picker>
+        </el-form-item>
+
+        <el-form-item label="地址" prop="addr">
+          <el-input placeholder="请输入地址" v-model="form.addr"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="cancel">取 消</el-button>
+        <el-button type="primary" @click="submit">确 定</el-button>
+      </span>
+    </el-dialog>
+    <div class="manage-header">
+      <el-button type="primary" @click="toggleVisible">+ 新增 </el-button>
+    </div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
+import { getUser } from "../api";
+
 export default {
   data() {
-    return {};
+    return {
+      dialogVisible: false,
+      form: {
+        name: "",
+        age: "",
+        sex: "",
+        birth: "",
+        addr: "",
+      },
+      rules: {
+        name: [
+          { required: true, message: "请输入姓名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 2 到 5 个字符", trigger: "blur" },
+        ],
+        age: [{ required: true, message: "请输入年龄", trigger: "blur" }],
+        sex: [{ required: true, message: "请选择性别", trigger: "change" }],
+        birth: [{ required: true, message: "请选择出生日期", trigger: "change" }],
+        addr: [{ required: true, message: "请输入地址", trigger: "blur" }],
+      },
+      tableData: [],
+    };
+  },
+  methods: {
+    toggleVisible() {
+      this.dialogVisible = !this.dialogVisible;
+    },
+    // 提交表单
+    submit() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          console.log(valid);
+          this.handleClose();
+        }
+      });
+    },
+    // 关闭弹窗前触发
+    handleClose() {
+      this.$refs.form.resetFields();
+      this.dialogVisible = false;
+    },
+    cancel() {
+      this.handleClose();
+    },
+  },
+  mounted() {
+    // 获取列表数据
+    getUser().then(({ data }) => {
+      console.log(data);
+    });
   },
 };
 </script>
+
+<style scoped>
+.manage {
+  height: 90vh;
+}
+</style>
